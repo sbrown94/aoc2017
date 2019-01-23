ae4bc2e21df0f86b8c515d75a9db4339c47c40ee
SBrown94 <scott.brown999@gmail.com>
2019-01-23 11:35:08 +0000

    added solution for day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -47,6 +47,29 @@ func (d Day) RunP1() error {
 	return nil
 }
 
+// RunP2 Part 2 of the problem
 func (d Day) RunP2() error {
+	input, err := common.ReadFromFileAsString("./day1/day1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	doubleInput := input + input
+	total := 0
+	for i, r := range input {
+		val, err := strconv.Atoi(string(doubleInput[(i + (len(input) / 2))]))
+		if err != nil {
+			return err
+		}
+		rangeVal, err := strconv.Atoi(string(r))
+		if err != nil {
+			return err
+		}
+		if rangeVal == val {
+			total += val
+		}
+	}
+
+	fmt.Println(total)
+
 	return nil
 }
